257ff8c908c7bf044539ed7b565ec29cee335932
leader <leaderman0114@gmail.com>
2021-08-06 23:21:31 +0500

    accounts app yaratildi, templates va static ulandi 001-dars tugadi

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    return render(request, 'accounts/customers.html')
