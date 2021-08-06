178204885482c47083027fef3460e3284989e3d7
leader <leaderman0114@gmail.com>
2021-08-07 00:36:10 +0500

    modellar qo'shildi, 002_1-dars tugadi

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Products, Order
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Products)
+admin.site.register(Order)
