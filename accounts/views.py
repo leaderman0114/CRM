b82a88825c35bd5209555ca59c1914b96974e83c
leader <leaderman0114@gmail.com>
2021-08-18 17:48:03 +0500

    formset and filter(search) are completed.005 end

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
-from accounts.forms import OrderForm
+from accounts.filters import OrderFilter
 from django.shortcuts import redirect, render
+from django.forms import inlineformset_factory
+from accounts.forms import OrderForm
 from .models import Customer, Order, Products
+from .filters import OrderFilter
 
 
 def home(request):
@@ -29,24 +32,32 @@ def customers(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     order_count = orders.count()
+
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
     context = {
         'customer': customer,
         'orders': orders,
         'order_count': order_count,
+        'myFilter': myFilter,
     }
     return render(request, 'accounts/customers.html',context)
 
 
 def create_order(request,pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
     customer = Customer.objects.get(id=pk)
-    form = OrderForm(initial={'customer': customer})
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        # form = formsetOrderForm(request.POST)
+        if formset.is_valid():
+            formset.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 
